195352a414c57f4e5702664dca75de0a545754cf
X Mister <someinkognito@gmail.com>
2023-04-28 03:04:16 +0300

    add contacts to local storage

--- src/components/App.js
+++ src/components/App.js
@@ -7,18 +7,30 @@ import { Contacts } from './Contacts/Contacts';
 import { nanoid } from 'nanoid';
 import { Title } from './Contacts/Contacts.styled';
 import { ContactsFilter } from './Filter/Filter';
-
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '+380-32-459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '+980-32-443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '180-32-645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '380-32-227-91-26' },
+];
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '+380-32-459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '+980-32-443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '180-32-645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '380-32-227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
-
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+    if (contacts) {
+      this.setState({ contacts });
+    } else {
+      this.setState({ contacts: initialContacts });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   addContact = ({ name, number }) => {
     const contact = {
       id: nanoid(),
